0646bcb14e1c3beeffd012f56c561c0743cf4b53
dario.silva <dario.silva@vilt-group.com>
2019-12-10 13:34:35 +0000

    Add tests.

--- src/test/resources/features/embratel.feature
+++ src/test/resources/features/embratel.feature
@@ -2,21 +2,53 @@
 # ------------------------------------------------------------------------------
 Feature: Search results
 
-  Scenario: Mostrar Home Embratel
+  Scenario: Mostrar Home page Embratel
     Given I'm at "https://www.embratel.com.br/"
     Then Site is ok
 
-  Scenario: Busca no Portal Sebrae
-    Given I'm at "https://www.sebrae.com.br/sites/PortalSebrae"
-    When Busca no Sebrae Nota Fiscal
-
   Scenario Outline: Abrir links do header
     Given I'm at "https://www.embratel.com.br/"
-    When I click on the header option "A Embratel"
-    Then I can see the title "A Embratel"
+    When I click on the header option "<option>"
+    Then I can see the title "<option>"
 
     Examples: 
       | option         |
       | A Embratel     |
       | Consultor      |
       | Espaço Cliente |
+
+  Scenario: Preencho o formulário
+    Given I'm at "https://www.embratel.com.br/"
+    When I click on the header option "Consultor"
+    And I fill the form with:
+      | Nome Completo                  | Minium Vilt                |
+      | Telefone com DDD               | 11 1234-56                 |
+      | Razão social da Empresa        | abcd                       |
+      | E-mail                         | minium.team@vilt-group.com |
+      | CPF ou CNPJ                    | 34.345.345/5345-6          |
+      | Horário para contato (período) | 19h - 22h                  |
+    Then I check that the form has the values:
+      | Nome Completo                  | Minium Vilt                |
+      | Telefone com DDD               | 11 1234-56                 |
+      | Razão social da Empresa        | abcd                       |
+      | E-mail                         | minium.team@vilt-group.com |
+      | CPF ou CNPJ                    | 34.345.345/5345-6          |
+      | Horário para contato (período) | 19h - 22h                  |
+
+  Scenario: Limpo o formulário preenchido
+    Given I'm at "https://www.embratel.com.br/"
+    When I click on the header option "Consultor"
+    And I fill the form with:
+      | Nome Completo                  | Minium Vilt                |
+      | Telefone com DDD               | 11 1234-56                 |
+      | Razão social da Empresa        | abcd                       |
+      | E-mail                         | minium.team@vilt-group.com |
+      | CPF ou CNPJ                    | 34.345.345/5345-6          |
+      | Horário para contato (período) | 19h - 22h                  |
+    And I click on the button "Limpar"
+    Then I check that the form has no values
+
+  Scenario: Busca no Google
+    Given I'm at "https://www.google.com"
+    When I search for "Embratel"
+    Then A link for "Embratel" is displayed
--- src/test/resources/steps/embratel.js
+++ src/test/resources/steps/embratel.js
@@ -1,30 +1,24 @@
 
-// TESTES DEFAULT DO TEMPLATE
+function specialFill(fld, value) {
+  fld.apply(function(value) { $(this).val(value); }, [ value ]);
+}
+
+
 Given(/^I'm at "([^"]*)"$/, function (url) {
   browser.get(url);
 });
 
-When(/^Busca no Sebrae (.*)$/, function (query) {
-  var button    = $("#btnBusca");
-  var searchbox = $(".search");
-  
-  searchbox.fill(query);
-  button.click();
-});
-
-/////////////////////////////////////////////////////////////////////////////
-//Testes Sites
-
 Then(/^Site is ok$/, function () {
-  // OK
   // Header
   var header = base.find(".header");
   expect(header.find(".list-menu-item").withText("A Embratel")).to.exist();
   expect(header.find(".list-menu-item").withText("Consultor")).to.exist();
   expect(header.find(".list-menu-item").withText("Blog")).to.exist();
+
   // Content
   var content = base.find(".main-content");
   expect(content.find(".sectionComponent")).to.have.size(7);
+
   // Footer
   var footer = base.find("#main-footer");
   expect(footer.find(".container-cliente").containingText("ESPAÇO CLIENTE")).to.exist();
@@ -37,12 +31,117 @@ When(/^I click on the header option "([^"]*)"$/, function(arg1) {
 });
 
 Then(/^I can see the title "([^"]*)"$/, function(arg1) {
-  expect(base.find(".main-title").containingText(arg1)).to.exist();
+  expect(base.find(".main-title, .main-section-title").containingText(arg1)).to.exist();
 });
 
-Then(/^Open (\w+) on menu Mobilidade$/, function(optionLabel) {
-  var optionLink = $("#nav-menu > div.row > div.container > div.row > ul.nav-global-list > li.nav-global-item:nth-child(2) > ul.nav-submenu > li.nav-submenu-item > ul.submenu-item-col > li > a").withText(optionLabel);
-  expect(optionLink).to.exist();
-  expect(optionLink).to.have.size(1);
-  browser.get(config.HOME_URL + optionLink.attr("href"));
-}); 
+When(/^I fill the form with:$/, function(datatable) {
+  var formVals = datatable.rowsHash();
+  var mainArea = base.find(".iframe").frames();
+  
+  for (var fldName in formVals) {
+    var value = String(formVals[fldName]);
+    
+    var label = mainArea.find("#r_txt1, #r_txt2, label").containingText(fldName);
+    
+    if (fldName === "Telefone com DDD") {
+      var valArray = value.split(" ");
+      var fldSelect = mainArea.find("select").below(label).first();
+      var fldInput = mainArea.find("input").below(label).first();
+      
+      fldSelect.select(valArray[0]);
+      specialFill(fldInput, valArray[1]);
+    } else if (fldName === "Horário para contato (período)") {
+      var fldInputs = mainArea.find(".questionario").find("label");
+      var input = fldInputs.withText(value).find("input");
+      input.check();
+    } else {
+      var fld = mainArea.find("input, select").below(label).first();
+      
+      if (fldName === "CPF ou CNPJ") {
+        specialFill(fld, value);
+      } else {
+        fld.fill(value);
+      }
+    }
+  }
+});
+
+Then(/^I check that the form has the values:$/, function(datatable) {
+  var formVals = datatable.rowsHash();
+  var mainArea = base.find(".iframe").frames();
+  
+  for (var fldName in formVals) {
+    var value = String(formVals[fldName]);
+    
+    var label = mainArea.find("#r_txt1, #r_txt2, label").containingText(fldName);
+    
+    if (fldName === "Telefone com DDD") {
+      var valArray = value.split(" ");
+      var fldSelect = mainArea.find("select").below(label).first();
+      var fldInput = mainArea.find("input").below(label).first();
+      
+      expect(String(fldSelect.waitForExistence().val())).to.be(valArray[0]);
+      expect(String(fldInput.waitForExistence().val())).to.be(valArray[1]);
+      
+    } else if (fldName === "Horário para contato (período)") {
+      var fldInputs = mainArea.find(".questionario").find("label");
+      var input = fldInputs.withText(value).find("input:checked");
+      expect(input).to.have.size(1);
+    } else {
+      var fld = mainArea.find("input, select").below(label).first();
+      expect(String(fld.waitForExistence().val())).to.be(value);
+    }
+  }
+});
+
+When(/^I click on the button "([^"]*)"$/, function(arg1) {
+  var frame = base.find(".iframe").frames();
+  frame.find("button").withValue(arg1).click();
+});
+
+Then(/^I check that the form has no values$/, function() {
+  var inputs = ["Nome Completo", "Telefone com DDD", "Razão social da Empresa", "E-mail", "CPF ou CNPJ", "Horário para contato (período)"];
+  var mainArea = base.find(".iframe").frames();
+
+  for (var input in inputs) {
+    var textLabel = inputs[input];
+    var label = mainArea.find("#r_txt1, #r_txt2, label").containingText(textLabel);
+    
+    if (textLabel === "Telefone com DDD") {
+      var fldSelect = mainArea.find("select").below(label).first();
+      var fldInput = mainArea.find("input").below(label).first();
+      
+      expect(String(fldSelect.waitForExistence().val())).to.be("");
+      expect(String(fldInput.waitForExistence().val())).to.be("");
+      
+    } else if (textLabel === "Horário para contato (período)") {
+      var fldInputs = mainArea.find(".questionario").find("label");
+      var checkboxes = fldInputs.find("input:checked");
+      expect(checkboxes).to.have.size(0);
+    } else {
+      var fld = mainArea.find("input, select").below(label).first();
+      expect(String(fld.waitForExistence().val())).to.be("");
+    }
+  }
+});
+
+When(/^I search for "([^"]*)"$/, function(arg1) {
+  var input = base.find("input").withName("q");
+  input.fill(arg1);
+  
+  var button = base.find("input").withName("btnK");
+  button.click();
+});
+
+Then(/^A link for "([^"]*)" is displayed$/, function (query) {
+  var links = $("a");
+  var linkUrls = config.searches[query];
+
+  expect(linkUrls).not.to.be.empty();
+
+  linkUrls.forEach(function (linkUrl) {
+    console.log(linkUrl);
+    var link = links.withAttr("data-href", linkUrl).add(links.withAttr("href", linkUrl));
+    expect(link).to.exist();
+  });
+});
